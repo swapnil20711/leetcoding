6ea85d0f95930851130df27f2e40879924f5702f
Swapnil <swapnil.b@getstan.app>
2025-11-06 11:41:08 +0530

    initial commit

---
+++ strings/max-frequency.js
@@ -0,0 +1,23 @@
+var maxFreqSum = function(s) {
+    let charMap = new Map();
+    let consonantMax = 0;
+    let vowelMax = 0;
+
+    let vowels = ['a','e','i','o','u']
+
+    for(let i=0;i<s.length;i++){
+        if(charMap.has(s[i])){
+            charMap.set(s[i],charMap.get(s[i])+1);
+        }else{
+            charMap.set(s[i],1)
+        }
+    }
+    for (let [key, value] of charMap) {
+      if(vowels.includes(key)){
+          vowelMax = Math.max(vowelMax,value)
+      }else{
+          consonantMax = Math.max(consonantMax,value)
+      }
+    }
+    return vowelMax+consonantMax;
+};
